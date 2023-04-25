32a823956cbb62a59c77049eb37706f6a4bf5950
Mehmet Tarhan <hi@memtarhan.com>
2023-04-25 15:26:09 +0100

    <SwiftUI>: Stacks

--- 5-create-user-interface-with-swiftui/Stacks.swiftpm/ContentView.swift
+++ 5-create-user-interface-with-swiftui/Stacks.swiftpm/ContentView.swift
@@ -41,6 +41,44 @@ struct ContentView: View {
                 }
             }
             .padding()
+            
+            ZStack {
+                Circle()
+                    .foregroundColor(Color.gray.opacity(0.2))
+                Circle()
+                    .scale(x: 0.75, y: 0.75)
+                    .foregroundColor(Color.yellow.opacity(0.5))
+                VStack {
+                    Image(systemName: "leaf.fill")
+                        .imageScale(.large)
+                        .foregroundColor(Color.green)
+                    Text("Hello, nature!")
+                    HStack {
+                        Button("One") {}
+                        Button("Two") {}
+                    }
+                }
+            }
+            .padding()
+            
+            ZStack {
+                Circle()
+                    .foregroundColor(Color.gray.opacity(0.2))
+                Circle()
+                    .scale(x: 0.75, y: 0.75)
+                    .foregroundColor(Color.yellow.opacity(0.5))
+                VStack {
+                    Image(systemName: "leaf.fill")
+                        .imageScale(.large)
+                        .foregroundColor(Color.green)
+                    Text("Hello, nature!")
+                    HStack {
+                        Button("One") {}
+                        Button("Two") {}
+                    }
+                }
+            }
+            .padding()
         }
         .padding()
     }
