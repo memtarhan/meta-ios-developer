df6ac91bcb5b3323b97e021d29ea012fc9e80d67
Mehmet Tarhan <hi@memtarhan.com>
2023-06-04 15:24:13 +0100

    [Advanced Swift] - Created Ingredient and MenuCategory enums

---
+++ 6-advanced-programming-in-swift/LittleLemon/LittleLemon/Ingredient.swift
@@ -0,0 +1,16 @@
+//
+//  Ingredient.swift
+//  LittleLemon
+//
+//  Created by Mehmet Tarhan on 04/06/2023.
+//
+
+import Foundation
+
+enum Ingredient: String {
+    case spinach
+    case broccoli
+    case carrot
+    case pasta
+    case tomatoSauce
+}
---
+++ 6-advanced-programming-in-swift/LittleLemon/LittleLemon/MenuCategory.swift
@@ -0,0 +1,14 @@
+//
+//  MenuCategory.swift
+//  LittleLemon
+//
+//  Created by Mehmet Tarhan on 04/06/2023.
+//
+
+import Foundation
+
+enum MenuCategory: String {
+    case food
+    case drink
+    case dessert
+}
