ede5c3cbbdb8f125a3814e7c487739e1c92bf5ce
Mehmet Tarhan <hi@memtarhan.com>
2023-06-04 18:17:16 +0100

    [Javascript] - Variables (Exercise)

--- 8-mobile-development-with-javascript/week-1/variables.js
+++ 8-mobile-development-with-javascript/week-1/variables.js
@@ -8,3 +8,13 @@ console.log(greeting, name)
 
 greeting = "Hey there!"
 console.log(greeting, name)
+
+
+var petDog = "Rex"
+var petCat = "Pepper"
+
+console.log(petDog)
+console.log(petCat)
+
+console.log("My pet dog's name is:", petDog)
+console.log("My pet cat's name is:", petCat)
