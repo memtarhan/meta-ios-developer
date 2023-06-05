da64bb383e4239595d5ead81e185a31dc2849a67
Mehmet Tarhan <hi@memtarhan.com>
2023-06-06 00:27:03 +0100

    [Javascript] - Loops

---
+++ 8-mobile-development-with-javascript/week-1/loops.js
@@ -0,0 +1,21 @@
+// for loop
+for (let i = 1; i <= 5; i++) {
+    console.log(i)
+}
+
+console.log("Counting completed")
+
+
+for (let i = 5; i > 0; i--) {
+    console.log(i)
+}
+
+console.log("Countdown completed")
+
+let i = 1
+while (i < 5) {
+    console.log(i)
+    i++
+}
+console.log("Counting completed")
+
