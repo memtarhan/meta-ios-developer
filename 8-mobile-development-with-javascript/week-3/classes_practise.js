91a4d709aa33488ad43388d28584937c481b9ba8
Mehmet Tarhan <hi@memtarhan.com>
2024-12-04 11:55:33 +0300

    [JavaScript] - Creating classes

---
+++ 8-mobile-development-with-javascript/week-3/classes_practise.js
@@ -0,0 +1,128 @@
+class Animal {
+    constructor(color = 'yellow', energy = 100) {
+        this.color = color;
+        this.energy = energy;
+    }
+
+    isActive() {
+        if (this.energy > 0) {
+            this.energy -= 20;
+            console.log('Energy is decreasing, currently at: ' + this.energy);
+
+        } else if (this.energy === 0) {
+            this.sleep();
+        }
+    }
+
+    sleep() {
+        this.energy += 20;
+        console.log('Energy is increasing, currently at: ' + this.energy);
+    }
+
+    getColor() {
+        console.log(this.color)
+    }
+}
+
+class Cat extends Animal {
+    constructor(sound = 'purr', canJumpHigh = true, canClimbTrees = true, color, energy) {
+        super(color, energy);
+        this.sound = sound;
+        this.canJumpHigh = canJumpHigh;
+        this.canClimbTrees = canClimbTrees;
+    }
+
+    makeSound() {
+        console.log(this.sound)
+    }
+}
+
+class Bird extends Animal {
+    constructor(sound = 'chirp', canFly = true, color, energy) {
+        super(color, energy);
+        this.sound = sound
+        this.canFly = canFly;
+    }
+
+    makeSound() {
+        console.log(this.sound)
+    }
+}
+
+/*
+Note: If I didn't use the super keyword in our sub-classes, once I'd run the above code, I'd get the following error:
+Uncaught ReferenceError: Must call super constructor in derived class before accessing 'this' or returning from
+derived constructor.
+
+ */
+
+class HouseCat extends Cat {
+    constructor(houseCatSound = "meow", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.houseCatSound = houseCatSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.houseCatSound);
+    }
+}
+
+class Tiger extends Cat {
+    constructor(tigerSound = "Roar!", sound,canJumpHigh,canClimbTrees, color,energy) {
+        super(sound,canJumpHigh,canClimbTrees, color,energy);
+        this.tigerSound = tigerSound;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        console.log(this.tigerSound);
+    }
+}
+
+class Parrot extends Bird {
+    constructor(canTalk = false, sound,canFly, color,energy) {
+        super(sound,canFly, color,energy);
+        this.canTalk = canTalk;
+    }
+    makeSound(option) {
+        if (option) {
+            super.makeSound();
+        }
+        if (this.canTalk) {
+            console.log("I'm a talking parrot!");
+        }
+    }
+}
+
+var polly = new Parrot(true); // we're passing `true` to the constructor so that polly can talk
+var fiji = new Parrot(false); // we're passing `false` to the constructor so that fiji can't talk
+
+polly.makeSound(); // 'chirp', 'I'm a talking parrot!'
+fiji.makeSound(); // 'chirp'
+
+polly.color; // yellow
+polly.energy; // 100
+
+polly.isActive(); // Energy is decreasing, currently at: 80
+
+var penguin = new Bird("shriek", false, "black and white", 200); // setting all the custom properties
+penguin; // Bird {color: 'black and white', energy: 200, sound: 'shriek', canFly: false }
+
+penguin.sound; // 'shriek'
+penguin.canFly; // false
+penguin.color; // 'black and white'
+penguin.energy; // 200
+penguin.isActive(); // Energy is decreasing, currently at: 180
+
+var leo = new HouseCat();
+// leo, no purring please:
+leo.makeSound(false); // meow
+// leo, both purr and meow now:
+leo.makeSound(true); // purr, meow
+
+var cuddles = new Tiger();
+cuddles.makeSound(false); // Roar!
+cuddels.makeSound(true); // purr, Roar!
