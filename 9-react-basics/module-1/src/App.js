a327882e0157616c96add5a288c6333ae2d8f3d2
Mehmet Tarhan <hi@memtarhan.com>
2024-12-05 22:39:39 +0300

    [React Basics] - multiple components

--- 9-react-basics/module-1/src/App.js
+++ 9-react-basics/module-1/src/App.js
@@ -1,13 +1,15 @@
-import Heading from "./Heading";
-import Promo from "./Promo";
+import "./App.css";
+import Card from "./Card";
 
 function App() {
     return (
         <div className="App">
-            <Heading firstName="Bob" lastName="Doe" message="Hello World"/>
-            <Promo heading="Testinggg" promoSubHeading="Hello World"/>
+            <h1>Task: Add three Card elements</h1>
+            <Card h2="First card's h2" h3="First card's h3" />
+            <Card h2="Second card's h2" h3="Second card's h3" />
+            <Card h2="Third card's h2" h3="Third card's h3" />
         </div>
     );
-}
+};
 
 export default App;
