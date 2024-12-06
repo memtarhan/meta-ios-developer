9f4518adb6b572198756fb377dd81db4bcf7c4f0
Mehmet Tarhan <hi@memtarhan.com>
2024-12-06 10:42:09 +0300

    [React Basics] - dynamic events

--- 9-react-basics/module-2/src/App.js
+++ 9-react-basics/module-2/src/App.js
@@ -1,10 +1,20 @@
 // import MyButton from "./MyButton";
-import ModeToggler from "./ModeToggler";
+// import ModeToggler from "./ModeToggler";
 
 function App() {
-  return (
-    <ModeToggler />
-  );
+    function handleClick() {
+        let randomNum = Math.floor(Math.random() * 3) + 1;
+        console.log(randomNum);
+        let userInput = prompt('type a number');
+        alert(`Computer number: ${randomNum}, Your guess: ${userInput}`);
+    }
+
+    return (
+        <div>
+            <h1>Task: Add a button and handle click event</h1>
+            <button onClick={handleClick}>Guess the number between 1 and 3</button>
+        </div>
+    );
 }
 
 export default App;
