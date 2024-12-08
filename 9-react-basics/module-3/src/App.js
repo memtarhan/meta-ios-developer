9295c66aa43ecb1ea4e85d7715a7f8fea2d76b33
Mehmet Tarhan <hi@memtarhan.com>
2024-12-09 00:59:33 +0300

    [React Basics] - media packages

--- 9-react-basics/module-3/src/App.js
+++ 9-react-basics/module-3/src/App.js
@@ -20,11 +20,42 @@ function App() {
     //
     //     </div>
     // );
+    // return (
+    //     <div>
+    //         <MyVideo />
+    //     </div>
+    // );
+
+    const bird1 = new Audio(
+        "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+    );
+
+    const bird2 = new Audio(
+        "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+    );
+
+    function toggle1() {
+        if (bird1.paused) {
+            bird1.play().then(r => {});
+        } else {
+            bird1.pause();
+        }
+    }
+
+    function toggle2() {
+        if (bird2.paused) {
+            bird2.play().then(r => {});
+        } else {
+            bird2.pause();
+        }
+    };
+
     return (
         <div>
-            <MyVideo />
+            <button onClick={toggle1}>Caspian Tern 1</button>
+            <button onClick={toggle2}>Caspian Tern 2</button>
         </div>
     );
-};
+}
 
 export default App;
