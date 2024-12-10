8537641f878c6d026918dcca0bdb74899e40dc42
Mehmet Tarhan <hi@memtarhan.com>
2024-12-10 14:56:13 +0300

    [React Native] - building a component

--- 10-react-native/module-1/my-app/App.js
+++ 10-react-native/module-1/my-app/App.js
@@ -1,12 +1,18 @@
 import {StatusBar} from 'expo-status-bar';
 import {StyleSheet, Text, View} from 'react-native';
 import LittleLemonHeader from "./components/LittleLemonHeader";
+import LittleLemonFooter from "./components/LittleLemonFooter";
 
 export default function App() {
     return (
-        <View style={{flex: 1, backgroundColor: '#fff'}}>
-            <LittleLemonHeader/>
-        </View>
+        <>
+            <View style={{flex: 1, backgroundColor: '#495E57'}}>
+                <LittleLemonHeader/>
+            </View>
+            <View style={{backgroundColor: '#495E57'}}>
+                <LittleLemonFooter/>
+            </View>
+        </>
     );
 }
 
