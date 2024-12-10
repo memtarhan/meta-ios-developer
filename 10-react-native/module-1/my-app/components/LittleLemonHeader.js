248dcd9553ec22fdb8c526a1866a1d2bfa2a0e44
Mehmet Tarhan <hi@memtarhan.com>
2024-12-10 15:04:27 +0300

    [React Native] - styling a component

--- 10-react-native/module-1/my-app/components/LittleLemonHeader.js
+++ 10-react-native/module-1/my-app/components/LittleLemonHeader.js
@@ -1,11 +1,14 @@
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 
 export default function LittleLemonHeader() {
     return (
-        <View style={{ flex: 0.2, backgroundColor: '#F4CE14' }}>
-            <Text style={{ padding: 40, fontSize: 30, color: 'black' }}>
-                Welcome to Little Lemon
+        <View style={{flex: 0.2, backgroundColor: '#F4CE14'}}>
+            <Text
+                style={{padding: 40, fontSize: 30, color: 'black'}}
+                numberOfLines={3}>
+                Welcome
+                <Text style={{fontWeight: 'bold'}}> Little Lemon</Text> {' '}
             </Text>
         </View>
     );
