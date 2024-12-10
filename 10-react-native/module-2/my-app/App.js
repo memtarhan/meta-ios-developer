9c6a312f976a82947901d6d6df9a833435ea33b7
Mehmet Tarhan <hi@memtarhan.com>
2024-12-10 19:56:41 +0300

    [React Native] - flat list

--- 10-react-native/module-2/my-app/App.js
+++ 10-react-native/module-2/my-app/App.js
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import MenuItems from "./components/MenuItems";
+import * as React from 'react';
+import {View, StyleSheet} from 'react-native';
+
+import LittleLemonHeader from './components/LittleLemonHeader';
+import LittleLemonFooter from './components/LittleLemonFooter';
+import MenuItems from './components/MenuItems';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <MenuItems />
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <>
+            <View style={styles.container}>
+                <LittleLemonHeader/>
+                <MenuItems/>
+            </View>
+            <View style={styles.footerContainer}>
+                <LittleLemonFooter/>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#333333',
+    },
+    footerContainer: {backgroundColor: '#333333'},
 });
