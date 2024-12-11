cf4d89139adc8a225fe341ccc65038a08710154a
Mehmet Tarhan <hi@memtarhan.com>
2024-12-11 16:35:00 +0300

    [React Native] - pressable

--- 10-react-native/module-3/my-app/App.js
+++ 10-react-native/module-3/my-app/App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import MenuItems from "./components/MenuItems";
+import LoginScreen from "./LoginScreen";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <MenuItems />
+      <LoginScreen />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000000',
     alignItems: 'center',
     justifyContent: 'center',
   },
