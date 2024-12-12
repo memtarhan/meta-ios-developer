784f4bac966ad7bcc96def6fede38a4b73eb5d51
Mehmet Tarhan <hi@memtarhan.com>
2024-12-12 10:59:34 +0300

    [React Native] - navigation drawer

--- 10-react-native/module-4/my-app/App.js
+++ 10-react-native/module-4/my-app/App.js
@@ -1,14 +1,12 @@
-import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-
+import { View, StyleSheet } from 'react-native';
 import LittleLemonHeader from './components/LittleLemonHeader';
 import LittleLemonFooter from './components/LittleLemonFooter';
 import WelcomeScreen from './Screens/WelcomeScreen';
 import LoginScreen from './Screens/LoginScreen';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 
-const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
 
 export default function App() {
     return (
@@ -16,10 +14,10 @@ export default function App() {
             <NavigationContainer>
                 <View style={styles.container}>
                     <LittleLemonHeader />
-                    <Stack.Navigator initialRouteName="Login">
-                        <Stack.Screen name="Welcome" component={WelcomeScreen} />
-                        <Stack.Screen name="Login" component={LoginScreen} />
-                    </Stack.Navigator>
+                    <Drawer.Navigator useLegacyImplementation initialRouteName="Login">
+                        <Drawer.Screen name="Welcome" component={WelcomeScreen} />
+                        <Drawer.Screen name="Login" component={LoginScreen} />
+                    </Drawer.Navigator>
                 </View>
                 <View style={styles.footerContainer}>
                     <LittleLemonFooter />
@@ -36,4 +34,3 @@ const styles = StyleSheet.create({
     },
     footerContainer: { backgroundColor: '#333333' },
 });
-
